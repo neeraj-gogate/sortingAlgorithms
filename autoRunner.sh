2adf594b98739dbcf41be92ba99709a0ee0a948c
Neeraj Gogate <neeraj.gogate@student.allenisd.org>
2022-11-18 22:41:58 -0600

    Adding Quick Sort

--- autoRunner.sh
+++ autoRunner.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm output.csv
 touch output.csv
-echo "Sort Name, Date & Time, Run Number, Branch Name, Git Hash, Command, Text File Name, Word Count, File Type, Execution Time (s), Average Time (s)" >> output.csv
+echo "Sort Name, Date & Time, Run Number, Branch Name, Git Hash, Command, Text File Name, Word Count, File Type, Execution Time (s), Execution Time (ms), Average Time (s)" >> output.csv
 for direct in Bubble-Sort Insertion-Sort Selection-Sort
 do
     cd $direct
@@ -21,8 +21,9 @@ do
 	    do
 		cat $list | head -n $count > temp.txt
 		time=$(TIMEFORMAT=%R; { time ./main < temp.txt > /dev/null; } 2>&1)
+		mile=$((time*1000))
 		date=$(date +"%D %T")
-		echo "$direct,$date,$j,main,REDO,time ./main < wordLists/$fileName-10e$x.txt > /dev/null, $fileName-10e$x.txt,$count,$fileName,$time" >> ../output.csv
+		echo "$direct,$date,$j,main,REDO,time ./main < wordLists/$fileName-10e$x.txt > /dev/null, $fileName-10e$x.txt,$count,$fileName,$time,$mile" >> ../output.csv
 	    done
 	done
     done
